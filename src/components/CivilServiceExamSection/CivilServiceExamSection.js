4744fc1a12824e334c3793095e4ec3c84409b0f4
weihaoqwq <2580297367@qq.com>
2023-12-28 09:54:29 +0800

    new

--- src/components/CivilServiceExamSection/CivilServiceExamSection.js
+++ src/components/CivilServiceExamSection/CivilServiceExamSection.js
@@ -1,49 +1,52 @@
-import React from 'react';
-import './CivilServiceExamSection.css';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
-
+import React from "react";
+import "./CivilServiceExamSection.css";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const CivilServiceExamSection = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { search } = useLocation();
   const queryParams = new URLSearchParams(search);
-  const jwt = queryParams.get('jwt');
+  const jwt = queryParams.get("jwt");
 
   return (
     <div className="civil-service-exam-section">
       {/* 公务员笔试标题 */}
-      <div className="exam-divider-text">
-        公务员笔试
-      </div>
-      
+      <div className="exam-divider-text">公务员笔试</div>
+
       {/* 圆角区域 */}
       <div className="exam-images">
         <div className="rounded-area">
-          <img onClick={() => navigate(`/Administrative?jwt=${jwt}`)} src={process.env.PUBLIC_URL + '/images/xingce.png'} alt="行测" />
+          <img
+            onClick={() => navigate(`/Administrative?jwt=${jwt}`)}
+            src={process.env.PUBLIC_URL + "/images/xingce.png"}
+            alt="行测"
+          />
           <div className="exam-buttons">
-            
-            <button onClick={() => navigate(`/Administrative?jwt=${jwt}`)} >行测</button>
-            
+            <button onClick={() => navigate(`/Administrative?jwt=${jwt}`)}>
+              行测
+            </button>
           </div>
         </div>
         <div className="rounded-area">
-          <img onClick={() => navigate(`/Deuteronomy?jwt=${jwt}`)} src={process.env.PUBLIC_URL + '/images/shenlun.png'} alt="申论" />
+          <img
+            onClick={() => navigate(`/Deuteronomy?jwt=${jwt}`)}
+            src={process.env.PUBLIC_URL + "/images/shenlun.png"}
+            alt="申论"
+          />
           <div className="exam-buttons">
-            
-            <button onClick={() => navigate(`/Deuteronomy?jwt=${jwt}`)}> 申论</button>
-            
+            <button onClick={() => navigate(`/Deuteronomy?jwt=${jwt}`)}>
+              {" "}
+              申论
+            </button>
           </div>
         </div>
       </div>
 
       {/* 更多资料标题 */}
-      <div className="exam-divider-text">
-        更多资料
-      </div>
+      {/* <div className="exam-divider-text">更多资料</div> */}
 
       {/* 四个圆角图片 */}
-      <div className="exam-images">
+      {/* <div className="exam-images">
         <div className="rounded-area">
           <img src={process.env.PUBLIC_URL + '/images/1.png'} alt="Image 1" />
         </div>
@@ -56,7 +59,7 @@ const CivilServiceExamSection = () => {
         <div className="rounded-area">
           <img src={process.env.PUBLIC_URL + '/images/4.png'} alt="Image 4" />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
--- src/page/BasicTest/BasicTest.js
+++ src/page/BasicTest/BasicTest.js
@@ -1,18 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import './BasicTest.css';
-import { useParams, useLocation, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./BasicTest.css";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 
 const ExamSystem = () => {
   const [questions, setQuestions] = useState([]);
   const [userAnswers, setUserAnswers] = useState([]);
   const { paperId } = useParams();
   const location = useLocation();
-  const { state: { paper } } = location;
+  const {
+    state: { paper },
+  } = location;
   const navigate = useNavigate();
 
   const { search } = useLocation();
   const queryParams = new URLSearchParams(search);
-  const jwt = queryParams.get('jwt');
+  const jwt = queryParams.get("jwt");
 
   useEffect(() => {
     // 处理未选择试卷的情况
@@ -25,8 +27,8 @@ const ExamSystem = () => {
       const initialUserAnswers = {};
       paper.questions.forEach((q) => {
         initialUserAnswers[q._id] = {
-          answerType: q.type === 'choice' ? 'choice' : 'text',
-          content: '',
+          answerType: q.type === "choice" ? "choice" : "text",
+          content: "",
         };
       });
       setUserAnswers(initialUserAnswers);
@@ -37,7 +39,7 @@ const ExamSystem = () => {
     setUserAnswers((prevAnswers) => ({
       ...prevAnswers,
       [questionId]: {
-        answerType: 'choice',
+        answerType: "choice",
         content: selectedOption,
       },
     }));
@@ -47,7 +49,7 @@ const ExamSystem = () => {
     setUserAnswers((prevAnswers) => ({
       ...prevAnswers,
       [questionId]: {
-        answerType: 'text',
+        answerType: "text",
         content: answer,
       },
     }));
@@ -58,20 +60,23 @@ const ExamSystem = () => {
       // 构建请求对象
       const requestBody = {
         paperID: paperId,
-        userID: 'user_id', // 你的用户 ID，这里需要替换为真实的用户 ID
+        userID: "user_id", // 你的用户 ID，这里需要替换为真实的用户 ID
         score: 0, // 这里可以根据需要计算分数
         answers: Object.values(userAnswers),
       };
 
       // 发送答案到后端
-      const response = await fetch('https://testsystem.zeabur.app/writtenTest/Answer/insert', {
-        method: 'POST',
-        headers: {
-          Authorization: jwt,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(requestBody),
-      });
+      const response = await fetch(
+        "https://testsystem.zeabur.app/writtenTest/Answer/insert",
+        {
+          method: "POST",
+          headers: {
+            Authorization: jwt,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestBody),
+        }
+      );
 
       if (!response.ok) {
         throw new Error(`Failed to submit answers. Status: ${response.status}`);
@@ -80,13 +85,16 @@ const ExamSystem = () => {
       // 处理提交成功后的操作，例如导航到下一个页面
       navigate(`/home?jwt=${jwt} `);
     } catch (error) {
-      console.error('提交答案时出错:', error.message);
+      console.error("提交答案时出错:", error.message);
     }
   };
 
   return (
     <div className="container">
-      <button onClick={() => navigate(`/home?jwt=${jwt}`)} className="back-button">
+      <button
+        onClick={() => navigate(`/home?jwt=${jwt}`)}
+        className="back-button"
+      >
         返回
       </button>
       <div className="title">公务员考试系统</div>
@@ -94,7 +102,7 @@ const ExamSystem = () => {
       {questions.map((q, index) => (
         <div key={q._id} className="paper">
           <div className="title">{`Question ${index + 1}: ${q.title}`}</div>
-          {q.questionType === 'choice' && (
+          {q.questionType === "choice" && (
             <ul className="choices">
               {q.choices.map((option, optionIndex) => (
                 <li key={optionIndex} className="choice">
@@ -110,11 +118,11 @@ const ExamSystem = () => {
               ))}
             </ul>
           )}
-          {q.questionType === 'text' && (
+          {q.questionType === "text" && (
             <textarea
               rows={4}
               placeholder="请输入您的答案"
-              value={userAnswers[q._id]?.content || ''}
+              value={userAnswers[q._id]?.content || ""}
               onChange={(e) => handleEssayAnswer(q._id, e.target.value)}
             />
           )}
